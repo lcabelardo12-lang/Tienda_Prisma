c251fa86598f1443a402e7ea0a22036f9b907711
lcabelardo12-lang <lcabelardo12@gmail.com>
2026-08-04 00:03:25 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'duava-prisma-v3';
+var CACHE_NAME = 'duava-prisma-v4';
 var urlsToCache = ['./index.html', './manifest.json', './logo.png'];
 
 self.addEventListener('install', function(event) {
